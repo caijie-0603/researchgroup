5e43d04ff217413fa56a7fe763a6aae496a86bea
Jeni <1563921381@qq.com>
2020-11-25 10:49:39 +0800

    Initial commit

---
+++ src/main/java/com/fifth/researchgroup/ResearchgroupApplication.java
@@ -0,0 +1,16 @@
+package com.fifth.researchgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lenovo
+ */
+@SpringBootApplication
+public class ResearchgroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchgroupApplication.class, args);
+    }
+
+}
